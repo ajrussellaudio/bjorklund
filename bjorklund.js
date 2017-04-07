721eea804cced8c3db0886e8e184484d0a31dbf4
Alan Russell <ajrussellaudio@gmail.com>
2017-04-07 19:23:54 +0100

    add extra functions

--- bjorklund.js
+++ bjorklund.js
@@ -1,5 +1,7 @@
-function bjorklund(slots, pulses) {
-  if (!pulses) return Array(slots);
+function Bjorklund() {}
+
+Bjorklund.distribute = function(slots, pulses) {
+  if (!pulses && pulses !== 0) return Array(slots);
   if (pulses > slots) pulses = slots;
   var divisor = slots - pulses;
   var remainders = [];
@@ -20,23 +22,38 @@ function bjorklund(slots, pulses) {
   return pattern;
 
   function buildString(level) {
-  if (level === -1) {
-    pattern.push(0)
-  } else if (level === -2) {
-    pattern.push(1)
-  } else {
-    for (var i = 0; i < counts[level]; i++) {
-      buildString(level-1);
+    if (level === -1) {
+      pattern.push(0)
+    } else if (level === -2) {
+      pattern.push(1)
+    } else {
+      for (var i = 0; i < counts[level]; i++) {
+        buildString(level-1);
+      }
+      if (remainders[level] !== 0) {
+        buildString(level-2);
+      }
     }
-    if (remainders[level] !== 0) {
-      buildString(level-2);
-    }
-  }
-}
+  };
 };
 
+Bjorklund.offset = function(array) {
+  if (!this.containsOne(array)) return array;
+  if (array[0] === 1) return array;
+  return this.offset(this.rotate(array));
+}
+
+Bjorklund.containsOne = function(array) {
+  return array.filter(function(item) {
+    return item === 1;
+  }).length > 0;
+}
+
+Bjorklund.rotate = function(array) {
+  array.push( array.shift() );
+  return array;
+}
 
 
 
-
-module.exports = bjorklund;
+module.exports = Bjorklund;
--- specs/bjorklund_spec.js
+++ specs/bjorklund_spec.js
@@ -1,35 +1,56 @@
 var assert = require('assert');
-var bjorklund = require('../bjorklund');
+var Bjorklund = require('../bjorklund');
 
-describe('Bjorklund', function() {
+describe('Bjorklund.distribute', function() {
 
   it("returns an array of given length", function() {
     var randomInt = Math.floor(Math.random() * 100);
-    assert.equal(bjorklund(randomInt).length, randomInt);
+    assert.equal(Bjorklund.distribute(randomInt).length, randomInt);
   });
 
   it("distributes 2 over 4", function() {
     var expected = [0,1,0,1];
-    assert.deepEqual(bjorklund(4, 2), expected);
+    assert.deepEqual(Bjorklund.distribute(4, 2), expected);
   });
 
   it("distributes 5 over 13", function() {
     var expected = [0,1,0,0,1,0,1,0,0,1,0,1,0];
-    assert.deepEqual(bjorklund(13, 5), expected);
+    assert.deepEqual(Bjorklund.distribute(13, 5), expected);
   });
 
   it("distributes 4 over 4", function() {
     var expected = [1,1,1,1];
-    assert.deepEqual(bjorklund(4, 4), expected);
+    assert.deepEqual(Bjorklund.distribute(4, 4), expected);
   });
 
   it("distributes 20 over 4", function() {
     var expected = [1,1,1,1];
-    assert.deepEqual(bjorklund(4, 20), expected);
+    assert.deepEqual(Bjorklund.distribute(4, 20), expected);
   });
 
   it("doesn't flip out when given 0 as input", function() {
-    assert.ok(bjorklund(7, 0));
+    var expected = [0,0,0,0,0,0,0];
+    assert.deepEqual(Bjorklund.distribute(7, 0), expected);
+  });
+
+});
+
+describe("Bjorklund.rotate", function() {
+  it("rotates an array by one", function() {
+    var expected = [0,1,0,0];
+    assert.deepEqual(Bjorklund.rotate([0,0,1,0]), expected);
   })
+});
+
+describe("Bjorklund.offset", function() {
+
+  it("offsets array to start with 1", function() {
+    var expected = [1, 0, 0, 0];
+    assert.deepEqual(Bjorklund.offset([0,0,1,0]), expected);
+  });
+
+  it("doesn't flip out if array doesn't contain 1", function() {
+    assert(Bjorklund.offset([0, 0, 0]));
+  });
 
 })
